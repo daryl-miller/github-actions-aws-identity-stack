ed89cf93210ab9e878bac66b334cfea752d86d58
Daryl Miller <daryl.miller@mondo.com.au>
2023-01-27 17:27:35 +1100

    Fix condition handling so roles deploy properly

--- lib/aws-identity-provider-github-stack.ts
+++ lib/aws-identity-provider-github-stack.ts
@@ -1,26 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
-import { Conditions, OpenIdConnectProvider, Role, WebIdentityPrincipal } from 'aws-cdk-lib/aws-iam';
+import { OpenIdConnectProvider, Role, OpenIdConnectPrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { repositoryIdentityConfig } from './config';
 
-const githubDomain = 'token.actions.githubusercontent.com';
-
-function buildCondition(config: repositoryIdentityConfig): Conditions {
-
-  const repoTarget = `repo:${config.owner}/${config.repository}:${config.branchFilter ?? 'main'}`
+function createGithubDeployRole(scope: Construct, githubOIDCProvider: OpenIdConnectProvider, config: repositoryIdentityConfig): void  {
   
-  return {
-    StringLike: {
-      [`${githubDomain}:sub`]: repoTarget,
-    },
-  };
-}
-
-function buildGithubDeployRole(scope: Construct, principal: WebIdentityPrincipal, config: repositoryIdentityConfig): Role {
   const {repository, managedPolicies, permissionsBoundary, policyStatements, owner} = config
   const name = `${owner}-${repository}`
   const role = new Role(scope, `${name}-GithubDeployRole`, {
-    assumedBy: principal,
+    assumedBy: new OpenIdConnectPrincipal(githubOIDCProvider, {
+      StringEquals: {
+        "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+      },
+      StringLike: {
+        "token.actions.githubusercontent.com:sub":
+        `repo:${config.owner}/${config.repository}:${config.branchFilter ?? '*'}`, 
+      },
+    }),
     roleName: `${name}-GithubDeployRole`,
     managedPolicies,
     permissionsBoundary,
@@ -28,24 +24,8 @@ function buildGithubDeployRole(scope: Construct, principal: WebIdentityPrincipal
     maxSessionDuration: cdk.Duration.hours(1),
   });
 
-  if (!policyStatements)
-    return role
-
-  policyStatements.map(policy => role.addToPolicy(policy))
-
-  return role
-}
-
-function createWebIdentity(scope: Construct, githubOIDCProvider: OpenIdConnectProvider, config: repositoryIdentityConfig): void  {
-  
-  const conditions = buildCondition(config)
-  const githubWebIdentityPrincipal = new WebIdentityPrincipal(
-    githubOIDCProvider.openIdConnectProviderArn,
-    conditions
-  )
-
-  buildGithubDeployRole(scope, githubWebIdentityPrincipal, config)
-
+  if (policyStatements)
+    policyStatements.map(policy => role.addToPolicy(policy))
 }
 
 export class AwsIdentityProviderGithubStack extends cdk.Stack {
@@ -53,12 +33,12 @@ export class AwsIdentityProviderGithubStack extends cdk.Stack {
     super(scope, id, props);
 
     const githubProvider = new OpenIdConnectProvider(this, 'githubProvider', {
-      url: `https://${githubDomain}`,
+      url: `https://token.actions.githubusercontent.com`,
       clientIds: ['sts.amazonaws.com'],
     });
 
     repositoryConfigs.map(config => {
-      createWebIdentity(this, githubProvider, config)
+      createGithubDeployRole(this, githubProvider, config)
     })
 
   }
